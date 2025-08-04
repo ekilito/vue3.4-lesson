648e874635c23f054552c23867329a238789c3c0
kilito <wudalong@3h1china.com>
2025-08-04 10:56:03 +0800

    createRenderer nodeOps

--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -1,1 +1,6 @@
-export {};
+export * from "@vue/reactivity";
+import { nodeOps } from "./nodeOps";
+import { patchProp } from "./patchProp";
+
+const renderOptions = Object.assign(nodeOps, patchProp);
+export const createRenderer = () => {};
---
+++ packages/runtime-dom/src/patchProp.ts
@@ -0,0 +1,2 @@
+// 主要是对节点元素的属性操作 class style event 等
+export const patchProp = () => {}
