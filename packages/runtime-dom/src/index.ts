de8df5431b2f35494c017550fa59b67e04432afd
kilito <wudalong@3h1china.com>
2025-08-04 14:52:57 +0800

    runtime-core

--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -1,10 +1,16 @@
 import { nodeOps } from "./nodeOps";
 import patchProp from "./patchProp";
+import { createRenderer } from '@vue/runtime-core'
 
 // 将节点操作和属性操作合并在一起
 const renderOptions = Object.assign({ patchProp }, nodeOps);
 
 export { renderOptions };
-export * from "@vue/reactivity";
 
+export const render = (vNode, container) => {
+  return createRenderer(renderOptions).render(vNode,container)
+}
+
+export * from "@vue/runtime-core"
+// runtime-dom => runtime-core => reactivity
 
